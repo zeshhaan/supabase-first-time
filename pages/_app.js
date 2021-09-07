06479979828e70132643717f9fa16b118d0b623d
Mohammed Zeeshan <34814964+zeeshanofficial@users.noreply.github.com>
2021-09-07 23:29:22 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import { supabase } from "../utils/supabaseClient";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,47 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 export default function Home() {
+  const [cards, setCards] = useState([]);
+  const [card, setCard] = useState({ name: "", card_type: "" });
+  const { name, card_type } = card;
+  useEffect(() => {
+    fetchCards();
+  }, []);
+  async function fetchCards() {
+    const { data } = await supabase.from("Cards").select();
+    setCards(data);
+    console.log("data: ", data);
+  }
+
+  async function createCard() {
+    await supabase.from("Cards").insert([{ name, card_type }]).single();
+    setCard({ name: "", card_type: "" });
+    fetchCards();
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <input
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setCard({ ...card, name: e.target.value })}
+          />
+          <input
+            placeholder="Card Type"
+            value={card_type}
+            onChange={(e) => setCard({ ...card, card_type: e.target.value })}
+          />
+          <button onClick={createCard}>Create Card</button>
+          {cards.map((card) => {
+            <div key={card.id}>
+              <h3>{card.name}</h3>
+              <p>{card.card_type}</p>
+            </div>;
+          })}
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
